3c94fee7ae259d57be264acae98a4ab78bf0fb84
Fung Chow <fungchow@gmail.com>
2022-03-22 05:12:40 +0800

    ex005: modified method isPangram to loop through String IntStream.

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,15 +1,26 @@
 package com.techreturners.exercise005;
 
+import java.util.stream.IntStream;
+import java.util.*;
+
 public class Exercise005 {
 
-    private final String[] LETTERLIST = {"a","b","c","d","e","f","h","i","j","k","l","m","n","o","p","q","r",
-                            "s","t","u","v","w","x","y","z"};
+    private ArrayList <Integer> letterList = new ArrayList<Integer>();
+
+    public Exercise005 (){
+        IntStream.range(97,123).forEach( n -> letterList.add(n)); // initialize letterList
+    }
 
     public boolean isPangram(String input) {
-        for (String aLetter: LETTERLIST)
-            if (input.toLowerCase().indexOf(aLetter) == -1)
-                return false;
-        return true;
+        if (input == null || input.trim().length() == 0)
+            return false;
+        int[] inputArray = input.toLowerCase().chars().toArray();
+        for (int i=0; i<inputArray.length; i++){
+            letterList.remove(new Integer(inputArray[i]));
+            if (letterList.size() == 0)
+                return true;
+        }
+        return false;
     }
 
 }
