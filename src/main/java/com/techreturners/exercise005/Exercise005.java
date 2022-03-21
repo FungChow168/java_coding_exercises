7a110a58149f479ed84fd53ecd916e92d4c565f0
Fung Chow <fungchow@gmail.com>
2022-03-22 01:13:28 +0800

    ex005: change constant name to uppercase.

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -2,11 +2,11 @@ package com.techreturners.exercise005;
 
 public class Exercise005 {
 
-    private final String[] letterList = {"a","b","c","d","e","f","h","i","j","k","l","m","n","o","p","q","r",
+    private final String[] LETTERLIST = {"a","b","c","d","e","f","h","i","j","k","l","m","n","o","p","q","r",
                             "s","t","u","v","w","x","y","z"};
 
     public boolean isPangram(String input) {
-        for (String aLetter: letterList)
+        for (String aLetter: LETTERLIST)
             if (input.toLowerCase().indexOf(aLetter) == -1)
                 return false;
         return true;
